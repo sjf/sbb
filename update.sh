05a735d1386b221fa18ebbce512945eca524e612
Sarah Fortune <sarah.fortune@gmail.com>
2025-01-01 20:10:39 -0800

    .

--- update.sh
+++ update.sh
@@ -4,9 +4,9 @@ set -uex
 cd ~/sbb
 if [ ! -f .venv/bin/activate ]; then
   python3 -m venv .venv
-  pip3 install -r requirements.txt
 fi
 . .venv/bin/activate
+pip3 install -r requirements.txt
 
 export MB_LOG_DIR=/home/sjf/logs
 export MB_LOG_FILE=sbb.log
