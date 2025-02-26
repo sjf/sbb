e311073752f5e3b1ec3133ef71cc6d98d8ce3df9
Sarah Fortune <sarah.fortune@gmail.com>
2025-02-26 03:02:57 -0800

    .

--- e2e_tests.sh
+++ e2e_tests.sh
@@ -3,5 +3,7 @@ set -eux
 export PYTEST=1
 if [[ $(hostname) == 'box' ]]; then
   export VERIFY_SSL=False
+elif [[ $(hostname) == 'nytspellingbeesolver.com' ]]; then
+  export BACKEND=https://beekey.buzz
 fi
 pytest e2e_test.py $@
