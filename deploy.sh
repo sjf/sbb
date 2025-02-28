b900ab6ef330a92a5eef8d6691e745189f7d6774
Sarah Fortune <sarah.fortune@gmail.com>
2025-02-28 11:41:14 -0800

    deploy ssss

--- deploy.sh
+++ deploy.sh
@@ -4,11 +4,14 @@ set -uxe
 cd ~/sbb
 git pull
 
+export FULL=True
+# Regenerate static site
+./update.sh generator
+
 # Rebuild all containers, use cached layers
 docker compose build --quiet backend
 docker compose up --no-deps -d --wait backend || dlogs backend
-# Regenerate static site
-./update.sh generator
+
 
 export BACKEND=https://beekey.buzz
 ./update.sh e2e
