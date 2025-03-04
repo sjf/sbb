853ca8e34ca7650b757e0b9cc5bb2f751875af2e
Sarah Fortune <sarah.fortune@gmail.com>
2025-03-03 17:55:37 -0800

    sss

--- setup_testdata.sh
+++ setup_testdata.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -u
 
+if [[ $(hostname) == 'nytspellingbeesolver.com' ]]; then
+  echo 'Not setting up test-data on prod' >&2
+  exit 1
+fi
+
 mkdir -p scraped/
 mkdir -p data/
 cp -v test-data/*.json scraped/
