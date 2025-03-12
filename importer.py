de06ac7495da41f27b4700eeafab2fdb434a3762
Sarah Fortune <sarah.fortune@gmail.com>
2025-03-12 00:34:20 -0700

    prints

--- importer.py
+++ importer.py
@@ -9,7 +9,7 @@ import requests_cache
 from requests.adapters import HTTPAdapter
 from dataclasses import dataclass, asdict, fields, field
 from urllib3.util.retry import Retry
-from typing import List, Any, Dict, Optional
+from typing import List, Any, Dict, Optional, Set
 from pyutils import *
 from pyutils.settings import config
 from hint_generator import HintGenerator
@@ -163,7 +163,7 @@ class Importer:
   def missing_answers(self, center_letter: str, outer_letters: str, answers: List[str]) -> List[str]:
     center_letter = center_letter.lower()
     outer_letters = outer_letters.lower()
-    def can_be_spelled(wordset, words, letter_set) -> List[str]:
+    def can_be_spelled(wordset: Set[str], words: List[str], letter_set: Set[str]) -> List[str]:
       if not center_letter in wordset:
         return []
       if wordset - letter_set == set():
@@ -175,13 +175,13 @@ class Importer:
     for ws,words in self.wordlist.all_words.items():
       valid_words.extend(can_be_spelled(ws, words, letter_set))
     missing_answers = minus(valid_words, answers)
-    # missing_from_dict = slist(answer_set - valid_words_set)
+    missing_from_dict = minus(answers, valid_words)
 
-    # print('Returned from corpus:', len(valid_words))
-    # print('Answers:', len(answers))
-    # print('Missing from corpus\n' + joinl(missing_from_dict))
-    # print('-------')
-    # print('Not in answers\n' + joinl(missing_answers))
+    print('Returned from corpus:', len(valid_words))
+    print('Answers:', len(answers))
+    print('Missing from corpus\n' + joinl(missing_from_dict))
+    print('-------')
+    print('Not in answers\n' + joinl(missing_answers))
     return missing_answers
 
 def get_clue_url(text: str) -> str:
